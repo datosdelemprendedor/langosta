d8b136a8b82764ddfa08f37f3e055bc3742f7275
cursosentelegram <cursosentelegram@gmail.com>
2026-03-22 17:41:52 -0300

    feat: add DesktopMain and TaskViewModel

---
+++ shared/src/commonMain/kotlin/com/langosta/mission/desktop/DesktopMain.kt
@@ -0,0 +1,24 @@
+package com.langosta.mission.desktop
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+import com.langosta.mission.data.api.OpenClawClient
+import com.langosta.mission.data.db.DatabaseManager
+import com.langosta.mission.data.repository.TaskRepository
+import com.langosta.mission.util.ConfigManager
+
+fun main() = application {
+    ConfigManager.loadDefaults()
+
+    val db = DatabaseManager()
+    val client = OpenClawClient(ConfigManager.getServerUrl())
+    val repository = TaskRepository(client, db)
+    val viewModel = TaskViewModel(repository)
+
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "OpenCLAW Mission Control"
+    ) {
+        App(viewModel)
+    }
+}
